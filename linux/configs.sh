8bd0d94d7926bcb6a7f02ec66ee7d37f23b33894
Chris Gibson <chris.david.gibson@gmail.com>
2015-03-27 19:45:12 -0500

    List all files instead of copying all to avoid scripts being copied

--- linux/configs.sh
+++ linux/configs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-cp ~/projects/dotfiles/linux/* ~/
+cp ~/projects/dotfiles/linux/.bash_aliases ~/.bash_aliases
+cp ~/projects/dotfiles/linux/.bash_profile ~/.bash_profile
+cp ~/projects/dotfiles/linux/.bashrc ~/.bashrc
+cp ~/projects/dotfiles/linux/.inputrc ~/.inputrc
+cp ~/projects/dotfiles/linux/.vimrc ~/.vimrc
+cp -r ~/projects/dotfiles/linux/.vim ~/
