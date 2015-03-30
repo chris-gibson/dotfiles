6f24dcf24ad1e8a6f7db4f5db7bfa9091710ded0
Chris Gibson <chris.david.gibson@gmail.com>
2015-03-30 13:20:41 -0500

    Add verbose copying

--- linux/configs.sh
+++ linux/configs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp ~/projects/dotfiles/linux/.bash_aliases ~/.bash_aliases
-cp ~/projects/dotfiles/linux/.bash_profile ~/.bash_profile
-cp ~/projects/dotfiles/linux/.bashrc ~/.bashrc
-cp ~/projects/dotfiles/linux/.inputrc ~/.inputrc
-cp ~/projects/dotfiles/linux/.vimrc ~/.vimrc
-cp -r ~/projects/dotfiles/linux/.vim ~/
+cp -v ~/projects/dotfiles/linux/.bash_aliases ~/.bash_aliases
+cp -v ~/projects/dotfiles/linux/.bash_profile ~/.bash_profile
+cp -v ~/projects/dotfiles/linux/.bashrc ~/.bashrc
+cp -v ~/projects/dotfiles/linux/.inputrc ~/.inputrc
+cp -v ~/projects/dotfiles/linux/.vimrc ~/.vimrc
+cp -vr ~/projects/dotfiles/linux/.vim ~/
