0865c7138ffcc43bb36ac7e9a8aa968568e68123
Chris <chris.david.gibson@gmail.com>
2015-04-03 10:49:18 -0500

    Add autoconf and build-essential

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install git awesome terminator
+sudo apt-get install git awesome terminator build-essential autoconf
 
 sudo cp -rv awesome/ ~/.config
 sudo cp -v background.png /usr/share/awesome/themes/default/
