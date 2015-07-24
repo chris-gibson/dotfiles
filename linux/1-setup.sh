ab03e17511d656fe50743bc72a6a593a57dcbb46
Chris <chris.david.gibson@gmail.com>
2015-07-23 20:08:37 -0500

    Add vim to install

--- linux/1-setup.sh
+++ linux/1-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install git awesome terminator build-essential autoconf
+sudo apt-get install git awesome terminator build-essential autoconf vim-gtk
 
 sudo cp -rv awesome/ ~/.config
 sudo cp -v background.png /usr/share/awesome/themes/default/
