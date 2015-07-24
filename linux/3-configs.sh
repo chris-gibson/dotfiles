898b9cf239a35ca78ca998cac9f19aef449bc941
Chris <chris.david.gibson@gmail.com>
2015-07-23 20:46:26 -0500

    Add all things to one file

--- linux/3-configs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp -v ~/projects/dotfiles/linux/.bash_aliases ~/.bash_aliases
-cp -v ~/projects/dotfiles/linux/.bash_profile ~/.bash_profile
-cp -v ~/projects/dotfiles/linux/.bashrc ~/.bashrc
-cp -v ~/projects/dotfiles/linux/.inputrc ~/.inputrc
-cp -v ~/projects/dotfiles/linux/.vimrc ~/.vimrc
-cp -vr ~/projects/dotfiles/linux/.vim ~/
