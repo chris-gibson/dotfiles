3b44a8ead97a8da25b390c5adde8ed615571cf92
Chris Gibson <chris.david.gibson@gmail.com>
2015-07-23 20:45:04 -0500

    Update 1-setup.sh

--- linux/1-setup.sh
+++ linux/1-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install git awesome terminator build-essential autoconf vim-gtk
+sudo apt-get install git awesome terminator build-essential autoconf vim-gtk silversearcher-ag
 
 sudo cp -rv awesome/ ~/.config
 sudo cp -v background.png /usr/share/awesome/themes/default/
